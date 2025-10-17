0a122ae767a6313a8f305a530a351d1fa63cbf88
Franz Ingólfsson <franz22@ru.is>
2025-10-17 16:53:19 +0000

    Created main interfaces

---
+++ core/ports.py
@@ -0,0 +1,33 @@
+
+
+from abc import ABC, abstractmethod
+from typing import Iterable, Any, Optional
+from datetime import datetime
+from .models import Assignment
+
+
+class ICanvasClient(ABC):
+    """For fetching Canvas data."""
+
+    @abstractmethod
+    def get_paginated(self, path: str, params: Optional[dict] = None) -> Iterable[Any]:
+        """Yield items from a paginated API endpoint."""
+        pass
+
+
+class IPresenter(ABC):
+    """Abstract interface for presenting output (like for console or JSON)."""
+
+    @abstractmethod
+    def display_courses(self, courses: list[dict[str, Any]]) -> None:
+        raise NotImplemented
+
+    @abstractmethod
+    def display_terms(self, terms: list[dict[str, Any]]) -> None:
+        raise NotImplemented
+
+    @abstractmethod
+    def display_assignments(self, overdue: list[Assignment], upcoming: list[Assignment]) -> None:
+        raise NotImplemented
+    
+    
